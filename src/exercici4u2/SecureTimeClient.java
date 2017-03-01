0eca4b5f6955b53bf83a7d7755df12831fbfe196
ericdote <rovx15@gmail.com>
2017-03-01 17:56:42 +0100

    FIN y Comentado

--- src/exercici4u2/SecureTimeClient.java
+++ src/exercici4u2/SecureTimeClient.java
@@ -1,6 +1,58 @@
 package exercici4u2;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.swing.JOptionPane;
+
 
 public class SecureTimeClient {
-    
+    //Tras crear los certificados y las keys SSL declaramos lo siguiente
+    //Declaramos el almacen de claves en las que confiamos
+    private static final String PROPIETARI="javax.net.ssl.trustStore";
+    //Damos la ruta de del almacen de claves
+    private static final String v_PROPIETARI="C:\\Users\\Eric\\Desktop\\DAM\\M9\\UF3\\Exercici4u2\\src\\exercici4u2\\SSL\\ClienteTrustStore.jks";    
+    //Declaramos el almacen de contraseñas en las que confiamos
+    private static final String PROPIETARI2="javax.net.ssl.trustStorePassword";
+    //Damos la contraseña del certificado
+    private static final String v_PROPIETARI2="123456";
+    static String HOST = "localhost";
+    static int PORT = 8745;
+    DataOutputStream outToServer;
+    BufferedReader bf;
+    //Declaramos SocketFactory y Socket, necesitamos ambos ya que si no, no podemos realizar el portocolo SSL
+    SocketFactory ssF;
+    Socket ss;
+
+    public SecureTimeClient(String host, int port) throws IOException {
+        //Establecemos los almacenes de trustore
+        System.setProperty(PROPIETARI, v_PROPIETARI);
+        System.setProperty(PROPIETARI2, v_PROPIETARI2);
+        //Creamos los socolos para conectarnos con servidor mediante SSL
+        ssF = (SSLSocketFactory)SSLSocketFactory.getDefault();
+        ss = (SSLSocket)ssF.createSocket(HOST, PORT);
+        outToServer = new DataOutputStream(ss.getOutputStream());
+        bf = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+        String dia = JOptionPane.showInputDialog(null, "Diu el numero del dia: ", "Entrando", 3);
+        String mes = JOptionPane.showInputDialog(null, "Diu el mes: ", "Entrando", 3);
+        String any = JOptionPane.showInputDialog(null, "Diu l'any: ", "Entrando", 3);
+        enviarAServer(Integer.parseInt(dia), Integer.parseInt(mes), Integer.parseInt(any));
+
+    }
+
+    public void enviarAServer(int dia, int mes, int any) throws IOException {
+        outToServer.writeBytes(dia + " " + mes + " " + any);
+        bf.close();
+        outToServer.close();
+        ss.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new SecureTimeClient(HOST, PORT);
+    }
 }
--- src/exercici4u2/SecureTimeServer.java
+++ src/exercici4u2/SecureTimeServer.java
@@ -1,5 +1,66 @@
 package exercici4u2;
 
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 public class SecureTimeServer {
-    
+    //Lo mismo que en el cliente
+    private static final String PROPIETARI = "javax.net.ssl.keyStore";
+    private static final String v_PROPIETARI = "C:\\Users\\Eric\\Desktop\\DAM\\M9\\UF3\\Exercici4u2\\src\\exercici4u2\\SSL\\servidorKey.jks";
+    private static final String PROPIETARI2 = "javax.net.ssl.keyStorePassword";
+    private static final String v_PROPIETARI2 = "123456";
+    private static final String[] nomDies = {"Diumenge", "Dilluns",
+        "Dimarts",
+        "Dimecres", "Dijous",
+        "Divendres",
+        "Dissabte"};
+    //Declaramos el ServerSocketFactory ya que es necesario para poder declarar el protocolo
+    private static ServerSocketFactory ssF;
+
+    public static void main(String[] argv) throws Exception {
+        try {
+            //Lo mismo que en el cliente
+            System.setProperty(PROPIETARI, v_PROPIETARI);
+            System.setProperty(PROPIETARI2, v_PROPIETARI2);
+            ssF = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+            ServerSocket srvSocket = (SSLServerSocket)ssF.createServerSocket(8745);
+
+            while (true) {
+                Socket ss = (SSLSocket)srvSocket.accept();
+                Scanner in = new Scanner(ss.getInputStream());
+                int[] data = new int[3];
+                boolean ok = true;
+                for (int i = 0; i < data.length; i++) {
+                    if (in.hasNextInt()) {
+                        data[i] = in.nextInt();
+                    } else {
+                        ok = false;
+                    }
+                }
+                PrintStream out = new PrintStream(ss.getOutputStream());
+                if (ok) {
+                    data[1] -= 1;
+                    GregorianCalendar cal = new GregorianCalendar(data[2],
+                            data[1], data[0]);
+                    int dia = cal.get(Calendar.DAY_OF_WEEK) - 1;
+                    System.out.println("Aquest dia era " + nomDies[dia] + ".");
+
+                } else {
+                    System.out.println("Format de les dades incorrecte.");
+                }
+                ss.close();
+            }
+        } catch (Exception ex) {
+            System.out.println("Error en les comuncacions: " + ex);
+        }
+    }
 }
